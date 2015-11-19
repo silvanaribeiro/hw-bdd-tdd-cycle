b1601eb1fba9bf54ebcbdf1cacbdd575a8cc4e0a
silvanaribeiro <smararib@uncc.edu>
2015-11-19 22:31:58 +0000

    Second step passing

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -3,8 +3,3 @@ Given /the following movies exist/ do |movies_table|
     Movie.create!(:title => movie["title"], :release_date => movie["release_date"], :rating => movie["rating"], :director => movie["director"])
   end
 end
-
-When /I go to the edit page for (.*)/ do |movie_name|
-  click_link("More about #{movie_name}")
-  click_link("Edit")
-end
