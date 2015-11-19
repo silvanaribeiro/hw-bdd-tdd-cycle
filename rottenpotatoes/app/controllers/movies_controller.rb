3f9061b3e2c3e13b05ff631a93343e3ad5863616
silvanaribeiro <smararib@uncc.edu>
2015-11-19 23:16:37 +0000

    First scenario done and all routes configured

--- rottenpotatoes/app/controllers/movies_controller.rb
+++ rottenpotatoes/app/controllers/movies_controller.rb
@@ -60,5 +60,6 @@ class MoviesController < ApplicationController
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
   end
+  
 
 end
--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -3,3 +3,9 @@ Given /the following movies exist/ do |movies_table|
     Movie.create!(:title => movie["title"], :release_date => movie["release_date"], :rating => movie["rating"], :director => movie["director"])
   end
 end
+
+Then /the director of "(.*)" should be "(.*)"/ do |movie, director|
+  director_exists = page.body.include?(director)
+  right_movie = page.body.include?(movie)
+  director_exists.should == true && right_movie.should == true
+end
